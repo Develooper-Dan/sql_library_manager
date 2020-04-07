c18be774277f6be950eeb60ce8b309d8f7048a5a
Daniel Grigo <daniel.grigo@freenet.de>
2020-04-07 17:41:14 +0200

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,17 @@
+//Setting up the database and connecting sequelize to it
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'library.db',
+  logging: false
+})
+
+const db = {
+  sequelize,
+  Sequelize,
+  models: {},
+};
+
+db.models.Book = require('./book.js')(sequelize);
+
+module.exports = db;
